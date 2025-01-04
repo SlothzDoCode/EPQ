5d4651a75d25d47cd95a6a25c23949faff11f515
Harry Harper <146091658+SlothzDoCode@users.noreply.github.com>
2025-01-04 11:00:33 +0000

    Update screen.py

--- documents/screen.py
+++ documents/screen.py
@@ -1,1 +1,17 @@
+import socket
+import tkinter as tk
+import threading
+import time
 
+#var's
+
+host = '192.168.1.76'
+port = 2325
+
+current_bg = "black"
+
+fg_dark = "black"
+fg_light = "white"
+
+positions = ["pos 1","pos 2","pos 3","pos 4","pos 5","pos 6","pos 7","pos 8","pos 9","pos 10","pos 11","pos 12","pos 13","pos 14","pos 15","pos 16"]
+countdown_timer = ["time5","time10","time15","time20","time25","time30","time35","time40","time45","time50","time55","time60","time65","time70","time75","time80","time85","time90","time95"]
